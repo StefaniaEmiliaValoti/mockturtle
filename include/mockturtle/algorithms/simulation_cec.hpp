a0f226c179235948cc9549c9261926909bec60e2
StefaniaEmiliaValoti <96013980+StefaniaEmiliaValoti@users.noreply.github.com>
2021-12-24 03:15:49 +0100

    Stefania_Valoti_CEC

--- include/mockturtle/algorithms/simulation_cec.hpp
+++ include/mockturtle/algorithms/simulation_cec.hpp
@@ -26,36 +26,69 @@
 /*!
   \file simulation_cec.hpp
   \brief Simulation-based CEC
-
   EPFL CS-472 2021 Final Project Option 2
 */
-
 #pragma once
-
 #include <kitty/constructors.hpp>
 #include <kitty/dynamic_truth_table.hpp>
 #include <kitty/operations.hpp>
-
 #include "../utils/node_map.hpp"
 #include "miter.hpp"
 #include "simulation.hpp"
-
 namespace mockturtle
 {
-
 /* Statistics to be reported */
 struct simulation_cec_stats
 {
   /*! \brief Split variable (simulation size). */
   uint32_t split_var{ 0 };
-
   /*! \brief Number of simulation rounds. */
+  uint32_t round{ 0 };
   uint32_t rounds{ 0 };
 };
-
 namespace detail
 {
-
+///////////////
+class new_simulator
+{
+public:
+  new_simulator() = delete;
+  new_simulator(uint32_t num_var, uint32_t split_variable, uint32_t turni) :
+       num_vars(num_vars), split_variable(split_variable), turni(turni) {}
+    //already in the normal code
+  kitty::dynamic_truth_table compute_constant( bool value ) const
+  {
+    kitty::dynamic_truth_table tt( split_variable);
+    return value ? ~tt : tt;    // depending if the value of value is true or false we have tt or tt negated bitwise.
+  }
+  kitty::dynamic_truth_table compute_pi( uint32_t index ) const
+  {
+    kitty::dynamic_truth_table tt(split_variable);
+     if(index < split_variable) {
+        kitty::create_nth_var(tt,index);}
+        else {
+             if (!((turni >> (index - split_variable)) & 1))
+                        tt=~tt;
+            }
+    return tt;
+  }
+  kitty::dynamic_truth_table compute_not( kitty::dynamic_truth_table const & value ) const
+  {
+    return ~value;
+  }
+//up already in the normal code except the if-else-if
+protected:
+  uint32_t num_vars;
+  uint32_t split_variable;
+  uint32_t turni;
+};
+/*! \brief Simulates truth tables.
+ *
+ * This simulator simulates truth tables.  Each primary input is assigned the
+ * projection function according to the index.  The number of variables must be
+ * known at compile time.
+ */
+//////////////
 template<class Ntk>
 class simulation_cec_impl
 {
@@ -63,33 +96,49 @@ public:
   using pattern_t = unordered_node_map<kitty::dynamic_truth_table, Ntk>;
   using node = typename Ntk::node;
   using signal = typename Ntk::signal;
-
 public:
   explicit simulation_cec_impl( Ntk& ntk, simulation_cec_stats& st )
       : _ntk( ntk ),
         _st( st )
   {
   }
-
   bool run()
   {
-    /* TODO: write your implementation here */
-    return false;
+	  if (_ntk.num_pis() <= 6) {
+		  _st.split_var = _ntk.num_pis();
+	  } else {
+		  uint32_t m = 7;
+		  for(; ; m++) {
+			  if (!(m < _ntk.num_pis() && (32 +(pow(2,(m-2))))*_ntk.size() <= pow(2, 29)))
+				  break;
+		  }
+		  _st.split_var = m;
+	  }
+	  _st.rounds = pow(2,_ntk.num_pis() -_st.split_var);
+	  _st.round = _st.rounds;
+	  bool Solution = sim();
+      return Solution;
   }
-
 private:
   /* you can add additional methods here */
-
+  bool sim() {
+                for(int k = 0; k < _st.round; k++){
+                    new_simulator new_simulator(_ntk.num_pis(),_st.split_var, k);
+                    const auto tts = simulate<kitty::dynamic_truth_table>(_ntk, new_simulator);
+                    for(auto& po : tts) {
+                        if(!kitty::is_const0(po))
+                            return false;
+                    }
+                }
+                return true;
+            }
 private:
   Ntk& _ntk;
   simulation_cec_stats& _st;
   /* you can add other attributes here */
 };
-
 } // namespace detail
-
 /* Entry point for users to call */
-
 /*! \brief Simulation-based CEC.
  *
  * This function implements a simulation-based combinational equivalence checker.
@@ -99,8 +148,7 @@ private:
  * if the network has more than 40 inputs.
  */
 template<class Ntk>
-std::optional<bool> simulation_cec( Ntk const& ntk1, Ntk const& ntk2, simulation_cec_stats* pst = nullptr )
-{
+std::optional<bool> simulation_cec( Ntk const& ntk1, Ntk const& ntk2, simulation_cec_stats* pst = nullptr ){
   static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
   static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
   static_assert( has_size_v<Ntk>, "Ntk does not implement the size method" );
@@ -109,26 +157,18 @@ std::optional<bool> simulation_cec( Ntk const& ntk1, Ntk const& ntk2, simulation
   static_assert( has_foreach_po_v<Ntk>, "Ntk does not implement the foreach_po method" );
   static_assert( has_foreach_node_v<Ntk>, "Ntk does not implement the foreach_node method" );
   static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
-
   simulation_cec_stats st;
-
   bool result = false;
-
   if ( ntk1.num_pis() > 40 )
     return std::nullopt;
-
   auto ntk_miter = miter<Ntk>( ntk1, ntk2 );
-
   if ( ntk_miter.has_value() )
   {
     detail::simulation_cec_impl p( *ntk_miter, st );
     result = p.run();
   }
-
   if ( pst )
     *pst = st;
-
   return result;
 }
-
 } // namespace mockturtle
